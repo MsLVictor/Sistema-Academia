5f05f0cd714b2ba6f7c156b5a7821f20a842f87d
MsLVictor <victormleite25@icloud.com>
2026-04-01 13:29:44 -0300

    feat/victor-leite: adiciona script, inserts e querys

--- SistemaAcademia/Repositories/UsuarioRepository.cs
+++ SistemaAcademia/Repositories/UsuarioRepository.cs
@@ -56,4 +56,86 @@ public class UsuarioRepository
         }
         return lista;
     }
+
+    public Usuario BuscarPorCpf(string cpf)
+    {
+        using(var conexao = new SqlConnection(_connectionString))
+        {
+            string sql = "SELECT Id, Nome, CPF, Email, Cargo FROM Usuario WHERE CPF = @cpf";
+
+            var cmd = new SqlCommand(sql, conexao);
+
+            cmd.Parameters.AddWithValue("@cpf", cpf);
+
+            conexao.Open();
+            using(var reader = cmd.ExecuteReader())
+            {
+                if(reader.Read())
+                    return MapearUsuario(reader);
+            }
+        }
+        return null;
+    }
+
+    public void Atualizar(Usuario usuario)
+    {
+        using(var conexao = new SqlConnection(_connectionString))
+        {
+            string sql = @"UPDATE Usuario
+                           SET Nome = @nome,
+                               Email = @Email,
+                               Cargo = @Cargo
+                           WHERE Id = @id";
+            
+            var comando = new SqlCommand(sql, conexao);
+
+            comando.Parameters.AddWithValue("@nome", usuario.Nome);
+            comando.Parameters.AddWithValue("@email", usuario.Email);
+            comando.Parameters.AddWithValue("@Cargo", usuario.CargoEnum.ToString());
+            comando.Parameters.AddWithValue("@id", usuario.Id);
+
+            conexao.Open();
+            comando.ExecuteNonQuery();
+        }
+    }
+
+    public void Excluir(int id)
+    {
+        using(var conexao = new SqlConnection(_connectionString))
+        {
+            string sql = "DELETE FROM Usuario WHERE Id = @id";
+
+            var comando = new SqlCommand(sql, conexao);
+
+            comando.Parameters.AddWithValue("@id", id);
+
+            conexao.Open();
+
+            int linhasAfetadas = comando.ExecuteNonQuery();
+
+            if(linhasAfetadas > 0)
+            {
+                Console.WriteLine("\n [SUCESSO] Usuário removido do sistema.");
+                return;
+            }  
+
+            Console.WriteLine("\n[AVISO] Nenhum usuário encontrado com esse ID.");
+            
+        }
+    }
+
+    private Usuario MapearUsuario(SqlDataReader reader)
+    {
+        string nome = reader["Nome"].ToString();
+        string cpf = reader["CPF"].ToString();
+        string email = reader["Email"].ToString();
+
+        CargosEnum cargo = System.Enum.Parse<CargosEnum>(reader["Cargo"].ToString(), true);
+
+        var usuario = new Usuario(nome, cpf, email, cargo);
+
+        usuario.Id = (int)reader["Id"];
+
+        return usuario;
+    }
 }
